544c4fd9f15e109a6e1d6b70db7a4707574188bf
Mauricio <Siumauricio@unitec.edu>
2022-09-29 01:34:06 -0600

    refactor: add layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,14 @@
-import {useTheme as useNextTheme} from 'next-themes';
-import {Switch, useTheme} from '@nextui-org/react';
 import type {NextPage} from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import {Layout} from '../components/layout';
+import {Content} from '../components/content';
 
 const Home: NextPage = () => {
-   const {setTheme} = useNextTheme();
-   const {isDark, type} = useTheme();
    return (
-      <div>
-         <Switch
-            checked={isDark}
-            onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
-         />
-      </div>
+      <Layout>
+         <Content />
+      </Layout>
    );
 };
 
