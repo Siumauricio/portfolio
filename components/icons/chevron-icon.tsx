58e14bb74e14a4f601a59b654fd2a7af15cf1f3b
Mauricio <Siumauricio@unitec.edu>
2022-09-29 01:33:49 -0600

    feat: add icons

---
+++ components/icons/chevron-icon.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Svg} from '../styles/svg';
+
+export const ChevronIcon = () => {
+   return (
+      <Svg
+         width="20"
+         height="20"
+         viewBox="0 0 24 24"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+         css={{
+            '& path': {
+               stroke: 'White',
+            },
+         }}
+      >
+         <path
+            d="M8.5 5L15.5 12L8.5 19"
+            stroke="#200E32"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+         />
+      </Svg>
+   );
+};
