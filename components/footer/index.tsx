5c82b0c8e79b4b99b07757e167b812b4cd71eeb4
Mauricio <Siumauricio@unitec.edu>
2022-10-01 05:11:55 -0600

    refactor: add bg color default

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,15 +1,16 @@
 import {Spacer, Text} from '@nextui-org/react';
 import React from 'react';
+import {Box} from '../styles/box';
 import {Flex} from '../styles/flex';
 
 export const Footer = () => {
    return (
-      <>
-         <Spacer y={2.5} />
+      <Box css={{bg: '$accents0'}}>
+         <Spacer css={{pt: '$16', mt: '0 !important'}} />
          <Flex justify={'center'}>
             <Text span>© 2022 Mauricio Siu. All Rights Reserved.</Text>
          </Flex>
          <Spacer y={2} />
-      </>
+      </Box>
    );
 };
