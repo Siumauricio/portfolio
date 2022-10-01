18d8408443720058cc7fd6669e5b5851de8d8044
Mauricio <Siumauricio@unitec.edu>
2022-10-01 03:11:28 -0600

    refactor: add content for about and projects page

--- components/content/home/index.tsx
+++ components/content/home/index.tsx
@@ -1,15 +1,13 @@
-import {Text, Spacer, Avatar, Card, Button, Link} from '@nextui-org/react';
-import Image from 'next/image';
+import {Text, Avatar, Card, Button, Link} from '@nextui-org/react';
 import {ChevronIcon} from '../../icons/chevron-icon';
 import {Box} from '../../styles/box';
 import {Flex} from '../../styles/flex';
-import img from '../../public/1.png';
 import {GithubIcon} from '../../icons/github-icon';
 import {LinkedinIcon} from '../../icons/linkedin-icon';
+import {SectionAnimation} from '../../animations/section';
 
 export const Home = () => (
-   <Box css={{'px': '$12', 'mt': '$8', '@xsMax': {px: '$10'}}}>
-      {/* <Image src={img.src} width={300} height={200} /> */}
+   <Box css={{'px': '$12', '@xsMax': {px: '$10'}}}>
       <Card variant="flat" css={{marginBottom: '$10'}}>
          <Card.Body css={{backgroundColor: '$accents2'}}>
             <Text css={{fontFamily: 'inherit', textAlign: 'center'}} as={'p'}>
@@ -33,111 +31,118 @@ export const Home = () => (
             pointer
             bordered
             size={'xl'}
-            src="https://www.craftz.dog/_next/image?url=%2Fimages%2Ftakuya.jpg&w=128&q=75"
+            src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
          />
       </Flex>
-      <Box css={{mb: '$10'}}>
-         <Text h3>Work</Text>
-         <Text
-            span
-            css={{textIndent: '1em', fontFamily: 'inherit', display: 'block'}}
-         >
-            Software Engineer with more than 4 years of experience in personal
-            projects in development of applications, with solid knowledge in the
-            programming area to deliver quality products and on time, I have a
-            great motivation to solve problems and I focus every day to learn
-            something new and improving on what I am passionate about.
-         </Text>
+      <SectionAnimation delay={0.1}>
+         <Box css={{mb: '$10'}}>
+            <Text h3>Work</Text>
+            <Text
+               span
+               css={{
+                  textIndent: '1em',
+                  fontFamily: 'inherit',
+                  display: 'block',
+               }}
+            >
+               Software Engineer with more than 4 years of experience in
+               personal projects in development of applications, with solid
+               knowledge in the programming area to deliver quality products and
+               on time, I have a great motivation to solve problems and I focus
+               every day to learn something new and improving on what I am
+               passionate about.
+            </Text>
 
-         <Flex justify={'center'} css={{mt: '$8'}}>
-            <Button iconRight={<ChevronIcon />}>My portfolio</Button>
-         </Flex>
-      </Box>
+            <Flex justify={'center'} css={{mt: '$8'}}>
+               <Button iconRight={<ChevronIcon />}>My portfolio</Button>
+            </Flex>
+         </Box>
+      </SectionAnimation>
 
-      <Box css={{mb: '$10'}}>
-         <Text h3>Bio</Text>
+      <SectionAnimation delay={0.2}>
+         <Box css={{mb: '$10'}}>
+            <Text h3>Bio</Text>
 
-         <Box as={'ul'} css={{m: 0}}>
-            <Box as={'li'} css={{textIndent: '-3.4em', pl: '3.4em'}}>
-               <Text span css={{mr: '1em'}} weight={'bold'}>
-                  1999
-               </Text>
-               <Text span>Born in Cortes, Honduras.</Text>
-            </Box>
-            <Box as={'li'} css={{textIndent: '-3.4em', pl: '3.4em'}}>
-               <Text span css={{mr: '1em'}} weight={'bold'}>
-                  2018 - 2022
-               </Text>
-               <Text span>
-                  Almost complete Bachelor&apos;s degree in Computer Science.
-               </Text>
-            </Box>
-            <Box as={'li'} css={{textIndent: '-3.4em', pl: '3.4em'}}>
-               <Text span css={{mr: '1em'}} weight={'bold'}>
-                  2022 - Present
-               </Text>
-               <Text span>Work as a Software Engineer at CodeExitos.</Text>
+            <Box as={'ul'} css={{m: 0}}>
+               <Box as={'li'} css={{textIndent: '-3.4em', pl: '3.4em'}}>
+                  <Text span css={{mr: '1em'}} weight={'bold'}>
+                     1999
+                  </Text>
+                  <Text span>Born in Cortes, Honduras.</Text>
+               </Box>
+               <Box as={'li'} css={{textIndent: '-3.4em', pl: '3.4em'}}>
+                  <Text span css={{mr: '1em'}} weight={'bold'}>
+                     2018 - 2022
+                  </Text>
+                  <Text span>
+                     Almost complete Bachelor&apos;s degree in Computer Science.
+                  </Text>
+               </Box>
+               <Box as={'li'} css={{textIndent: '-3.4em', pl: '3.4em'}}>
+                  <Text span css={{mr: '1em'}} weight={'bold'}>
+                     2022 - Present
+                  </Text>
+                  <Text span>Work as a Software Engineer at CodeExitos.</Text>
+               </Box>
             </Box>
          </Box>
-      </Box>
+      </SectionAnimation>
 
-      <Box css={{mb: '$10'}}>
-         <Text h3>I ❤️ </Text>
-         <Text as={'span'} css={{textIndent: '1em', fontFamily: 'inherit'}}>
-            I love to learn new things, I am passionate about technology and I
-            always try to learn something new every day, I love to solve
-            problems and I am always looking for new challenges.
-         </Text>
-      </Box>
+      <SectionAnimation delay={0.3}>
+         <Box css={{mb: '$10'}}>
+            <Text h3>I ❤️ </Text>
+            <Text as={'span'} css={{textIndent: '1em', fontFamily: 'inherit'}}>
+               I love to learn new things, I am passionate about technology and
+               I always try to learn something new every day, I love to solve
+               problems and I am always looking for new challenges.
+            </Text>
+         </Box>
+      </SectionAnimation>
 
-      <Box css={{mb: '$10'}}>
-         <Text h3>On the Web </Text>
-         <Box as={'ul'} css={{m: 0}}>
-            <Box as={'li'}>
-               <Link
-                  block
-                  target={'_blank'}
-                  color="primary"
-                  href="https://github.com/Siumauricio"
-               >
-                  <Button
-                     light
+      <SectionAnimation delay={0.3}>
+         <Box css={{mb: '$10'}}>
+            <Text h3>On the Web </Text>
+            <Box as={'ul'} css={{m: 0}}>
+               <Box as={'li'}>
+                  <Link
+                     block
+                     target={'_blank'}
                      color="primary"
-                     auto
-                     icon={<GithubIcon />}
-                     ripple={false}
-                     css={{pl: '$3'}}
+                     href="https://github.com/Siumauricio"
                   >
-                     @Siumauricio
-                  </Button>
-               </Link>
-            </Box>
-            <Box as={'li'}>
-               <Link
-                  block
-                  target={'_blank'}
-                  color="primary"
-                  href="https://www.linkedin.com/in/carlos-mauricio-ortiz-siu-6b9011184/"
-               >
-                  <Button
-                     light
+                     <Button
+                        light
+                        color="primary"
+                        auto
+                        icon={<GithubIcon />}
+                        ripple={false}
+                        css={{pl: '$3'}}
+                     >
+                        @Siumauricio
+                     </Button>
+                  </Link>
+               </Box>
+               <Box as={'li'}>
+                  <Link
+                     block
+                     target={'_blank'}
                      color="primary"
-                     auto
-                     icon={<LinkedinIcon />}
-                     ripple={false}
-                     css={{pl: '$3'}}
+                     href="https://www.linkedin.com/in/carlos-mauricio-ortiz-siu-6b9011184/"
                   >
-                     @Mauricio Siu
-                  </Button>
-               </Link>
+                     <Button
+                        light
+                        color="primary"
+                        auto
+                        icon={<LinkedinIcon />}
+                        ripple={false}
+                        css={{pl: '$3'}}
+                     >
+                        @Mauricio Siu
+                     </Button>
+                  </Link>
+               </Box>
             </Box>
          </Box>
-      </Box>
-
-      <Spacer y={2.5} />
-      <Flex justify={'center'}>
-         <Text span>© 2022 Mauricio Siu. All Rights Reserved.</Text>
-      </Flex>
-      <Spacer y={2} />
+      </SectionAnimation>
    </Box>
 );
--- components/content/projects/index.tsx
+++ components/content/projects/index.tsx
@@ -1,5 +1,107 @@
+import {Card, Col, Link, Row, Text} from '@nextui-org/react';
+import Image from 'next/image';
 import React from 'react';
+import {SectionAnimation} from '../../animations/section';
+import {Box} from '../../styles/box';
+import {Flex} from '../../styles/flex';
+import {GridContainer} from '../../styles/grid';
+import {projects} from './utils';
 
 export const Projects = () => {
-   return <div>index</div>;
+   return (
+      <Box>
+         <Text h3 css={{'@xsMax': {textAlign: 'center'}}}>
+            Work
+         </Text>
+         <GridContainer
+            cols={{
+               '@xs': 2,
+               '@xsMax': 1,
+            }}
+            gap={'xl'}
+            align="start"
+         >
+            {projects.map((project, index) => (
+               <SectionAnimation
+                  key={index}
+                  delay={project.delay}
+                  CSS={{
+                     height: '100%',
+                     borderRadius: '14px',
+                     backgroundColor: 'transparent',
+                  }}
+               >
+                  <Card
+                     isPressable
+                     css={{
+                        height: '100%',
+                        display: 'block',
+                     }}
+                  >
+                     <Card.Body
+                        css={{
+                           p: 0,
+                           flex: 'auto',
+                        }}
+                     >
+                        <Card.Image
+                           src={project.img}
+                           objectFit="cover"
+                           width="100%"
+                           height={140}
+                           alt={project.title}
+                        />
+                     </Card.Body>
+                     <Card.Footer css={{justifyItems: 'flex-start'}}>
+                        <Row wrap="wrap" justify="space-between" align="center">
+                           <Text b>{project.title}</Text>
+                           <Text
+                              css={{
+                                 color: '$accents7',
+                                 fontWeight: '$semibold',
+                                 fontSize: '$sm',
+                                 fontFamily: 'inherit',
+                              }}
+                           >
+                              {project.description}
+                           </Text>
+                        </Row>
+                     </Card.Footer>
+                  </Card>
+               </SectionAnimation>
+            ))}
+         </GridContainer>
+      </Box>
+   );
 };
+// <Card
+//    isHoverable
+//    isPressable
+//    key={index}
+//    css={{
+//       '& img': {borderRadius: '$md'},
+//       'maxW': '230px',
+//       'mb': '$10',
+//    }}
+// >
+//    <Image
+//       src={project.img}
+//       width={230}
+//       height={130}
+//       alt={project.alt}
+//    />
+//    <Text
+//       as={'p'}
+//       css={{
+//          textAlign: 'center',
+//          fontFamily: 'inherit',
+//          mt: '$4',
+//          fontSize: '$xl',
+//       }}
+//    >
+//       {project.title}
+//    </Text>
+//    <Text css={{fontSize: '14px', fontFamily: 'inherit'}}>
+//       {project.description}
+//    </Text>
+// </Card>
