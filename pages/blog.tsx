d746dc27fec11af3cfa88d707971a7278fe03e1e
Mauricio <Siumauricio@unitec.edu>
2022-11-08 20:36:48 -0600

    removed

--- pages/blog.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {ArticleLayout} from '../components/layout/article-layout';
-
-const Index = () => {
-   return <ArticleLayout>blog</ArticleLayout>;
-};
-
-export default Index;
