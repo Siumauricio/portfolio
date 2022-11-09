be20ea933bf08c230ed4067f87ccf54f182b919f
Mauricio <Siumauricio@unitec.edu>
2022-11-08 20:35:44 -0600

    feat: add base components for mdx

---
+++ blog/components/index.tsx
@@ -0,0 +1,35 @@
+import {Link, Text} from '@nextui-org/react';
+
+export const components = {
+   h1: (props: any) => <Text h1 {...props} css={{mb: '$6'}} />,
+   h2: (props: any) => <Text h2 {...props} />,
+   h3: (props: any) => <Text h3 {...props} />,
+   h4: (props: any) => <Text h4 {...props} />,
+   h5: (props: any) => <Text h5 {...props} />,
+   h6: (props: any) => <Text h6 {...props} />,
+   p: (props: any) => (
+      <Text
+         as="p"
+         css={{
+            fontFamily: 'inherit',
+            my: '$9',
+            lineHeight: '$xl',
+            letterSpacing: '$normal',
+         }}
+      >
+         {props.children}
+      </Text>
+   ),
+   a: (props: any) => {
+      const isRelativeUrl = !/^([a-z0-9]*:|.{0})\/\/.*$/gim.test(props.href);
+      return (
+         <Link
+            href={props.href}
+            target={isRelativeUrl ? '_self' : '_blank'}
+            css={{width: 'fit-content', display: 'initial'}}
+         >
+            {props.children}
+         </Link>
+      );
+   },
+};
