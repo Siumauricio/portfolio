b72b1e64cbab4d05315400cfbb6086e296785f45
Mauricio Siu <siumauricio@hotmail.com>
2023-06-23 00:44:37 -0600

    feat: add project

--- components/content/projects/utils.ts
+++ components/content/projects/utils.ts
@@ -1,5 +1,18 @@
 export const projects = [
   {
+    title:
+      "E-commerce Website for a local business that manage their sells trought whatsaap",
+    description: `A fully website for a local business that can sells products online with a payment system and a dashboard for the admin to manage the products`,
+    img: "/Fithes.png",
+    href: "/projects/ecommerce-fithes",
+    preview: "https://fithes.com/",
+    github: "https://github.com/Siumauricio/rippleui",
+    alt: "Projects",
+    platform: "Web",
+    stack: "TailwindCSS, Nextjs, Typescript, NextUI.",
+    delay: 0.1,
+  },
+  {
     title: "Design System TailwindCSS (Ripple UI)",
     description: `A complete design system for a website that can be used for any website with a dark and light theme and a lot of components premade based TailwindCSS.`,
     img: "/RippleUI.png",
--- pages/projects/[project].tsx
+++ pages/projects/[project].tsx
@@ -1,138 +1,135 @@
-import {NextPage, NextPageContext} from 'next';
-import {Badge, Button, Link, Text} from '@nextui-org/react';
-import {useRouter} from 'next/router';
-import React, {useEffect, useState} from 'react';
-import {projects} from '../../components/content/projects/utils';
-import {ArticleLayout} from '../../components/layout/article-layout';
-import {Flex} from '../../components/styles/flex';
-import NextLink from 'next/link';
-import {GetStaticProps} from 'next';
-import {ChevronIcon} from '../../components/icons/chevron-icon';
-import Image from 'next/image';
-import {Box} from '../../components/styles/box';
-import {LinkIcon} from '../../components/icons/link-icon';
-import {GithubIcon} from '../../components/icons/github-icon';
+import { NextPage, NextPageContext } from "next";
+import { Badge, Button, Link, Text } from "@nextui-org/react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { projects } from "../../components/content/projects/utils";
+import { ArticleLayout } from "../../components/layout/article-layout";
+import { Flex } from "../../components/styles/flex";
+import NextLink from "next/link";
+import { ChevronIcon } from "../../components/icons/chevron-icon";
+import Image from "next/image";
+import { Box } from "../../components/styles/box";
+import { LinkIcon } from "../../components/icons/link-icon";
+import { GithubIcon } from "../../components/icons/github-icon";
 
-type Project = typeof projects[0];
+type Project = (typeof projects)[0];
 
 interface Props {
-   project: Project;
+  project: Project;
 }
 
-const Index = ({project}: Props) => {
-   return (
-      <ArticleLayout title={project.title}>
-         <Flex
-            direction={'column'}
+const Index = ({ project }: Props) => {
+  return (
+    <ArticleLayout title={project.title}>
+      <Flex
+        direction={"column"}
+        css={{
+          gap: "$6",
+          "@xsMax": {
+            px: "$10",
+          },
+        }}
+      >
+        <Box css={{ "& img": { borderRadius: "$lg" } }}>
+          <Image
+            src={project.img}
+            width={640}
+            alt={project.alt}
+            objectFit="cover"
+            height={296}
+            quality={100}
+          />
+        </Box>
+
+        <Flex
+          css={{ gap: "$6", "& path": { stroke: "$accents9" } }}
+          align={"center"}
+        >
+          <NextLink href={"/projects"}>
+            <Link
+              css={{ fontSize: "$xl", fontWeight: "$semibold" }}
+              color={"primary"}
+            >
+              Projects
+            </Link>
+          </NextLink>
+          <ChevronIcon />
+          <Text
+            h3
             css={{
-               'gap': '$6',
-               '@xsMax': {
-                  px: '$10',
-               },
+              "@xsMax": { textAlign: "center" },
+              fontSize: "$xl",
+              my: 0,
             }}
-         >
-            <Box css={{'& img': {borderRadius: '$lg'}}}>
-               <Image
-                  src={project.img}
-                  width={640}
-                  alt={project.alt}
-                  objectFit="cover"
-                  height={296}
-                  quality={100}
-               />
-            </Box>
+          >
+            {project.title}
+          </Text>
+        </Flex>
+        <Text
+          span
+          css={{
+            display: "block",
+          }}
+        >
+          {project.description}
+        </Text>
+        <Flex css={{ gap: "$6" }} align={"center"}>
+          <Badge
+            variant="flat"
+            color={"primary"}
+            css={{ height: "fit-content", border: "none" }}
+          >
+            Platform
+          </Badge>
+          <Text span>{project.platform}</Text>
+        </Flex>
 
-            <Flex
-               css={{'gap': '$6', '& path': {stroke: '$accents9'}}}
-               align={'center'}
-            >
-               <NextLink href={'/projects'}>
-                  <Link
-                     css={{fontSize: '$xl', fontWeight: '$semibold'}}
-                     color={'primary'}
-                  >
-                     Projects
-                  </Link>
-               </NextLink>
-               <ChevronIcon />
-               <Text
-                  h3
-                  css={{
-                     '@xsMax': {textAlign: 'center'},
-                     'fontSize': '$xl',
-                     'my': 0,
-                  }}
-               >
-                  {project.title}
-               </Text>
-            </Flex>
-            <Text
-               span
-               css={{
-                  display: 'block',
-               }}
-            >
-               {project.description}
-            </Text>
-            <Flex css={{gap: '$6'}} align={'center'}>
-               <Badge
-                  variant="flat"
-                  color={'primary'}
-                  css={{height: 'fit-content', border: 'none'}}
-               >
-                  Platform
-               </Badge>
-               <Text span>{project.platform}</Text>
-            </Flex>
+        <Flex css={{ gap: "$6" }} align={"center"}>
+          <Badge
+            variant="flat"
+            color={"primary"}
+            css={{ height: "fit-content", border: "none" }}
+          >
+            Stack
+          </Badge>
+          <Text span>{project.stack}</Text>
+        </Flex>
 
-            <Flex css={{gap: '$6'}} align={'center'}>
-               <Badge
-                  variant="flat"
-                  color={'primary'}
-                  css={{height: 'fit-content', border: 'none'}}
-               >
-                  Stack
-               </Badge>
-               <Text span>{project.stack}</Text>
-            </Flex>
+        <Link
+          block
+          color="primary"
+          css={{ gap: "$6" }}
+          href={project.preview}
+          target="_blank"
+        >
+          Live preview <LinkIcon width={21} height={22} color="$primary" />
+        </Link>
 
-            <Link
-               block
-               color="primary"
-               css={{gap: '$6'}}
-               href={project.preview}
-               target="_blank"
-            >
-               Live preview <LinkIcon width={21} height={22} color="$primary" />
-            </Link>
-
-            <Link
-               block
-               color="primary"
-               css={{gap: '$6'}}
-               href={project.github}
-               target="_blank"
-            >
-               Github repo <GithubIcon />
-            </Link>
-         </Flex>
-      </ArticleLayout>
-   );
+        <Link
+          block
+          color="primary"
+          css={{ gap: "$6" }}
+          href={project.github}
+          target="_blank"
+        >
+          Github repo <GithubIcon />
+        </Link>
+      </Flex>
+    </ArticleLayout>
+  );
 };
 
 Index.getInitialProps = async (ctx: NextPageContext) => {
-   const projectExists = projects.find(
-      (project) => project.href === ctx.asPath
-   );
-   if (!projectExists && ctx.res) {
-      ctx.res.writeHead(302, {
-         Location: '/',
-      });
-      ctx.res.end();
-   }
-   return {
-      project: projectExists,
-   };
+  const projectExists = projects.find((project) => project.href === ctx.asPath);
+  if (!projectExists && ctx.res) {
+    ctx.res.writeHead(302, {
+      Location: "/",
+    });
+    ctx.res.end();
+  }
+  return {
+    project: projectExists,
+  };
 };
 
 export default Index;
